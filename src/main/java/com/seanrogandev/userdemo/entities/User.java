408a0745f0debb07aa615477446a480beb46829a
seanrogan <srogan88@gmail.com>
2022-06-11 14:09:30 -0400

    MVP finished

--- src/main/java/com/seanrogandev/userdemo/entities/User.java
+++ src/main/java/com/seanrogandev/userdemo/entities/User.java
@@ -1,9 +1,7 @@
 package com.seanrogandev.userdemo.entities;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,11 +11,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Objects;
 
 @Entity
-@Table(name="users" , schema="user_demo")
 @Getter
 @Setter
 @ToString
